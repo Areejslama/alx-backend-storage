1942df1e8692b838caffcf90480aab5cdd34f67a
Areejslama <areegahmed449@gmail.com>
2024-08-10 01:06:23 +0300

    4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,10 @@
 -- this script to define trigger
+delimiter //
 CREATE TRIGGER value AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
+	UPDATE items
 	SET @quantity = @quantity - NEW.quantity_orderd
-END;
+	WHERE name = NEW.item_name;
+END;//
+delimiter;
