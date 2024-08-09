9d011506258f15f29ae4a863c07797374b62ff18
Areejslama <areegahmed449@gmail.com>
2024-08-10 02:03:25 +0300

    5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,9 +4,11 @@ DELIMITER //
 CREATE TRIGGER account BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-	UPDATE valid_email
-	SET username = CONCAT(username, NEW.email)
-	WHERE name = NEW.valid_email;
+	 IF OLD.email != NEW.email THEN
+		SET NEW.valid_email = 0;
+	ELSE
+		SET NEW.valid_email = NEW.valid_email;
+	END IF;
 END;
 //
 
