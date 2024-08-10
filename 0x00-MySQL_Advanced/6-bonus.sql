1ad0caa1a757cda33f5ca0bfb89a6389b10e39ca
Areejslama <areegahmed449@gmail.com>
2024-08-10 03:07:01 +0300

    6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,26 +1,29 @@
 -- this script to create stored procedure
 DELIMITER //
 
-CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score FLOAT)
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255),score FLOAT
+)
 BEGIN
-	DECLARE project_id INT DEFAULT 0;
-	DECLARE project_number INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+    DECLARE project_number INT DEFAULT 0;
 
-	SELECT COUNT(id) 
-	INTO project_number
-	FROM projects
-	WHERE name = project_name
-	IF  project_number = 0 THEN
-		INSERT INTO projects(name)
-		VALUES(project_name)
-	END IF;
+    SELECT COUNT(id)
+    INTO project_number
+    FROM projects
+    WHERE name = project_name;
 
-	SELECT id
-	INTO project_id
-	FROM projects
-	WHERE name = project_name
-	INSERT INTO corrections(user_id,  project_name, score)
-	VALUES(user_id,  project_name, score)
+    IF project_number = 0 THEN
+        INSERT INTO projects(name)
+        VALUES(project_name);
+    END IF;
+
+    SELECT id
+    INTO project_id
+    FROM projects
+    WHERE name = project_name;
+
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES(user_id, project_id, score);
 
 END //
 
