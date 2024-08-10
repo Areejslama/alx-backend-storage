8740d70633d4e4878d73092a1f96fdec5edc4421
Areejslama <areegahmed449@gmail.com>
2024-08-10 16:59:37 +0300

    7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,14 +4,20 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id DECIMAL)
 BEGIN
 	DECLARE project_id INT DEFAULT 0;
-	
-	SELECT id
-	INTO project_id
-	FROM projects
-	WHERE name = project_name;
+	DECLARE avg_score FLOAT;
 
-	INSERT INTO users(user_id, project_id)
-	VALUES(user_id, project_id)
+	SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+
+	SELECT AVG(avg_score)
+	INTO average_score
+	FROM users
+	WHERE user_id = user_id;
+
+        INSERT INTO correction(user_id, project_id, score)
+        VALUES(user_id, project_id, avg_score)
 END //
 
 DELIMITER ;
