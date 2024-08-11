92cbd6f130be355120f87a3291da4ef8afd331fb
Areejslama <areegahmed449@gmail.com>
2024-08-11 04:10:00 +0300

    11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,6 @@
 CREATE VIEW need_meeting 
 AS SELECT name, score, last_meeting
 FROM students
-WHERE score < 80 AND last_meeting IS NULL;
-OR (last_meeting IS NOT NULL AND last_meeting < CURDATE() - INTERVAL 1 MONTH);
+WHERE score < 80 
+AND (last_meeting IS NULL 
+	OR last_meeting < CURDATE() - INTERVAL 1 MONTH);
