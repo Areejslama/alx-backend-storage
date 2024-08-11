dc8c0ec23ef807dacaeb6c87470c50067eda78ca
Areejslama <areegahmed449@gmail.com>
2024-08-11 04:16:23 +0300

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- this script to sort bands
-SELECT band_name, (split - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 0) - IFNULL(formed, 0))
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
