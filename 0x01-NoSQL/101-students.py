9bffd26c9721947d87bbede154f8968f052ee079
Areejslama <areegahmed449@gmail.com>
2024-08-14 03:38:37 +0300

    101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,7 +2,7 @@
 """define function"""
 
 
-def top_students(mongo_collection):
-    """function"""
-    result = mongo_collection.find({"averageScores" : "averageScore"})
+def top_students(mongo_collection, top_n=5):
+    """Retrieve and sort the top N students by average score in descending order."""
+    result = mongo_collection.find({}).sort("averageScore", -1).limit(top_n)
     return result
