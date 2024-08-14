a0e64b62e9229a89c17b2f24c6ee0caa2be69003
Areejslama <areegahmed449@gmail.com>
2024-08-14 03:51:28 +0300

    101

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
-"""define function"""
+"""Define function"""
 
 
-def top_students(mongo_collection, top_n=5):
-    """Retrieve and sort the top N students by average score in descending order."""
-    result = mongo_collection.find({}).sort("averageScore", -1).limit(top_n)
+def top_students(mongo_collection):
+    """Retrieve and sort the top average score in descending order."""
+    pipeline = [
+        {
+            '$project': {
+                '_id': 1,
+                'name': 1,
+                'averageScore': {
+                    '$avg': '$topics.score'
+                }
+            }
+        },
+        {
+            '$sort': {'averageScore': -1}
+        },
+    ]
+    result = mongo_collection.aggregate(pipeline)
     return result
+
