0cc1159baa1610afbb251a69e3dc16f3735e1af9
Areejslama <areegahmed449@gmail.com>
2024-08-17 00:13:34 +0300

    0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -66,18 +66,18 @@ class Cache:
         return data
 
     def replay(func: callable):
-        """define function"""
-         key = func.__qualname__
+        """Replays the history"""
+        key = func.__qualname__
 
-    data1 = self._redis.lrange(f"{key}:inputs", 0, -1)
-    data2 = self._redis.lrange(f"{key}:outputs", 0, -1)
+        data1 = self._redis.lrange(f"{key}:inputs", 0, -1)
+        data2 = self._redis.lrange(f"{key}:outputs", 0, -1)
 
-    print(f"{key} was called {len(data1)} times:")
-    
-    for k, v in zip(data1, data2):
-        val = '{}(*{}) -> {}'.format(
-            key,
-            k.decode('utf-8'),
-            v.decode('utf-8')
-        )
-        print(val)
+        print(f"{key} was called {len(data1)} times:")
+
+        for k, v in zip(data1, data2):
+            val = '{}(*{}) -> {}'.format(
+                    key,
+                    k.decode('utf-8'),
+                    v.decode('utf-8')
+                    )
+            print(val)
